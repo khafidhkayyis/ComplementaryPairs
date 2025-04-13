9dc3ff797ba44b806535a4d4caa43a8db8072c3f
khafidhkayyis <khafidhkayyis@gmail.com>
2025-04-13 08:13:27 +0700

    finishing

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 let stringData = ["abc", "acb", "ab", "a"];
+function countComplementaryPairs(stringData) {
+    // Normalisasi String ke Dalam Bentuk Terurut
+    const sorted = []
+    for (let i = 0; i < stringData.length; i++){
+        sorted.push(stringData[i].split('').sort().join(''))
+    } 
+    // Urutkan Semua String Terurut Tadi
+    sorted.sort()
+    // Mencari Pasangan Komplementer
+    let count = 0;
+    const seen = {}
+    // Perulangan Ganda untuk Membandingkan Semua Pasangan
+    for (let i = 0; i< sorted.length; i++){
+        for(let j = 0; j< sorted.length; j++){
+            // Lewati Jika Indeks Sama atau Sudah Pernah Dicek
+            if(i == j) continue
+            if(seen[sorted[i]] == sorted[j] || seen[sorted[j]] == sorted[i]) 
+            continue
+            // Cek Apakah Dua String Hanya Beda 1 Huruf atau Tidak
+            if(sorted[i].length < sorted[j].length){
+                if(sorted[j].replace(sorted[i],'').length <= 1){
+                    seen[sorted[i]] = sorted[j]
+                    count++
+                }
+            }else{
+                if(sorted[i].replace(sorted[j],'').length <= 1){
+                    seen[sorted[i]] = sorted[j]
+                    count++
+                }
+            }
+        }
+    }
+    // Kembalikan Total Pasangan   
+    return count
+}
 
-const sorted = []
-for (let i = 0; i < stringData.length; i++){
-    sorted.push(stringData[i].split('').sort().join(''))
-} 
-sorted.sort()
-console.log(sorted)
 
-// let count = 0;
-// const seen = {}
-
-// for (let i = 0; i< sorted.length; i++){
-//     for(let j = 0; j< sorted.length; j++){
-
-//     }
-// }
+// console.log("Result:", countComplementaryPairs(stringData))
